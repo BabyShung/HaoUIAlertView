f53fabb03c8b612b2832c4a04df043b3828425aa
Hao Zheng <babyshung8@gmail.com>
2014-12-18 13:41:56 -0500

    Initial Commit

---
+++ HaoUIAlertViewFianl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HaoUIAlertViewFianl
+//
+//  Created by Hao Zheng on 12/18/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HaoUIAlertViewFianl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HaoUIAlertViewFianl
+//
+//  Created by Hao Zheng on 12/18/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
